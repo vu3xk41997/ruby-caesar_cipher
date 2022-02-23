e81f8a753607fb253572ad56950c964bb12689fe
Mojito <vu3xk41997@gmail.com>
2022-02-23 17:07:38 +0100

    project done

---
+++ caesar_cipher.rb
@@ -0,0 +1,20 @@
+def caesar_cipher(string, factor)
+    split_string = string.split('')
+    string_ord_modified = []
+    split_string.map do |char| 
+        if char =~ /[A-Za-z]/
+            if char == char.upcase
+                new_char = ((char.ord + 5 - 65) % 26 + 65).chr
+                string_ord_modified.push(new_char)
+            else
+                new_char = ((char.ord + 5 - 97) % 26 + 97).chr
+                string_ord_modified.push(new_char)
+            end
+        else
+            string_ord_modified.push(char)
+        end
+    end
+    return string_ord_modified.join('')  
+end
+
+caesar_cipher("What a string!", 5)
