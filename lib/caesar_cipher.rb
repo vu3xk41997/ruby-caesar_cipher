3003db671dd5f987a4143bc01d16911fce452a1c
Mojito <vu3xk41997@gmail.com>
2022-04-16 13:12:07 +0200

    implement rspec into the method for TDD

---
+++ lib/caesar_cipher.rb
@@ -0,0 +1,20 @@
+class CaesarCipher
+    def caesar_cipher(string, factor)
+        split_string = string.split('')
+        string_ord_modified = []
+        split_string.map do |char| 
+            if char =~ /[A-Za-z]/
+                if char == char.upcase
+                    new_char = ((char.ord + factor - 65) % 26 + 65).chr
+                    string_ord_modified.push(new_char)
+                else
+                    new_char = ((char.ord + factor - 97) % 26 + 97).chr
+                    string_ord_modified.push(new_char)
+                end
+            else
+                string_ord_modified.push(char)
+            end
+        end
+        return string_ord_modified.join('')  
+    end
+end
---
+++ spec/caesar_cipher_spec.rb
@@ -0,0 +1,10 @@
+require './lib/caesar_cipher.rb'
+
+describe CaesarCipher do
+    describe "#caesar_cipher" do
+        it "Take in a string and the shift factor and then outputs the modified string." do
+            caesarcipher = CaesarCipher.new
+            expect(caesarcipher.caesar_cipher("What a string!", 5)).to eql("Bmfy f xywnsl!")
+        end
+    end
+end
